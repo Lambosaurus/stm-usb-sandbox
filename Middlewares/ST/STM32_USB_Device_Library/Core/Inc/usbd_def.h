5b7ce6a3a085978be311015b13af0c84b3c704c5
Tim Lamborn <tlamborn@gmail.com>
2021-03-30 00:52:06 +1300

    Trimming the bloat

--- Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
+++ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
@@ -1,39 +1,10 @@
-/**
-  ******************************************************************************
-  * @file    usbd_def.h
-  * @author  MCD Application Team
-  * @brief   General defines for the usb device library
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; Copyright (c) 2015 STMicroelectronics.
-  * All rights reserved.</center></h2>
-  *
-  * This software component is licensed by ST under Ultimate Liberty license
-  * SLA0044, the "License"; You may not use this file except in compliance with
-  * the License. You may obtain a copy of the License at:
-  *                      www.st.com/SLA0044
-  *
-  ******************************************************************************
-  */
-
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef __USBD_DEF_H
 #define __USBD_DEF_H
 
 #include <stdint.h>
 #include <string.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-
-/*  Device Status */
-#define USBD_STATE_DEFAULT                              0x01U
-#define USBD_STATE_ADDRESSED                            0x02U
-#define USBD_STATE_CONFIGURED                           0x03U
-#define USBD_STATE_SUSPENDED                            0x04U
+#include "usb\USB_Defs.h"
 
 
 /*  EP0 State */
@@ -46,59 +17,6 @@ extern "C" {
 #define USBD_EP0_STALL                                  0x06U
 
 
-
-typedef  struct  usb_setup_req
-{
-  uint8_t   bmRequest;
-  uint8_t   bRequest;
-  uint16_t  wValue;
-  uint16_t  wIndex;
-  uint16_t  wLength;
-} USBD_SetupReqTypedef;
-
-struct _USBD_HandleTypeDef;
-
-typedef struct _Device_cb
-{
-  uint8_t (*Init)(struct _USBD_HandleTypeDef *pdev, uint8_t cfgidx);
-  uint8_t (*DeInit)(struct _USBD_HandleTypeDef *pdev, uint8_t cfgidx);
-  /* Control Endpoints*/
-  uint8_t (*Setup)(struct _USBD_HandleTypeDef *pdev, USBD_SetupReqTypedef  *req);
-  uint8_t (*EP0_TxSent)(struct _USBD_HandleTypeDef *pdev);
-  uint8_t (*EP0_RxReady)(struct _USBD_HandleTypeDef *pdev);
-  /* Class Specific Endpoints*/
-  uint8_t (*DataIn)(struct _USBD_HandleTypeDef *pdev, uint8_t epnum);
-  uint8_t (*DataOut)(struct _USBD_HandleTypeDef *pdev, uint8_t epnum);
-  uint8_t (*SOF)(struct _USBD_HandleTypeDef *pdev);
-  uint8_t (*IsoINIncomplete)(struct _USBD_HandleTypeDef *pdev, uint8_t epnum);
-  uint8_t (*IsoOUTIncomplete)(struct _USBD_HandleTypeDef *pdev, uint8_t epnum);
-
-  uint8_t  *(*GetHSConfigDescriptor)(uint16_t *length);
-  uint8_t  *(*GetFSConfigDescriptor)(uint16_t *length);
-  uint8_t  *(*GetOtherSpeedConfigDescriptor)(uint16_t *length);
-  uint8_t  *(*GetDeviceQualifierDescriptor)(uint16_t *length);
-#if (USBD_SUPPORT_USER_STRING_DESC == 1U)
-  uint8_t  *(*GetUsrStrDescriptor)(struct _USBD_HandleTypeDef *pdev, uint8_t index,  uint16_t *length);
-#endif
-
-} USBD_ClassTypeDef;
-
-/* Following USB Device Speed */
-typedef enum
-{
-  USBD_SPEED_HIGH  = 0U,
-  USBD_SPEED_FULL  = 1U,
-  USBD_SPEED_LOW   = 2U,
-} USBD_SpeedTypeDef;
-
-/* Following USB Device status */
-typedef enum
-{
-  USBD_OK   = 0U,
-  USBD_BUSY,
-  USBD_FAIL,
-} USBD_StatusTypeDef;
-
 /* USB Device handle structure */
 typedef struct
 {
@@ -110,23 +28,12 @@ typedef struct
 /* USB Device handle structure */
 typedef struct _USBD_HandleTypeDef
 {
-  USBD_SpeedTypeDef       dev_speed;
   USBD_EndpointTypeDef    ep_in[16];
   USBD_EndpointTypeDef    ep_out[16];
   uint32_t                ep0_state;
   uint32_t                ep0_data_len;
-  uint8_t                 dev_state;
-  uint32_t                dev_remote_wakeup;
-
-  USBD_ClassTypeDef       *pClass;
 } USBD_HandleTypeDef;
 
-
-
-#ifdef __cplusplus
-}
 #endif
-
-#endif /* __USBD_DEF_H */
 
 

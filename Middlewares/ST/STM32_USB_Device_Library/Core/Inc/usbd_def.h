e4b3bdd4fcb2a43d4a63da63ce86e9a5deaf3d1b
Tim Lamborn <tlamborn@gmail.com>
2021-03-30 23:30:29 +1300

    Clobbering cludge

--- Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
+++ Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h
@@ -7,16 +7,6 @@
 #include "usb\USB_Defs.h"
 
 
-/*  EP0 State */
-#define USBD_EP0_IDLE                                   0x00U
-#define USBD_EP0_SETUP                                  0x01U
-#define USBD_EP0_DATA_IN                                0x02U
-#define USBD_EP0_DATA_OUT                               0x03U
-#define USBD_EP0_STATUS_IN                              0x04U
-#define USBD_EP0_STATUS_OUT                             0x05U
-#define USBD_EP0_STALL                                  0x06U
-
-
 /* USB Device handle structure */
 typedef struct
 {
@@ -30,8 +20,7 @@ typedef struct _USBD_HandleTypeDef
 {
   USBD_EndpointTypeDef    ep_in[16];
   USBD_EndpointTypeDef    ep_out[16];
-  uint32_t                ep0_state;
-  uint32_t                ep0_data_len;
+  //uint32_t                ep0_data_len;
 } USBD_HandleTypeDef;
 
 #endif
